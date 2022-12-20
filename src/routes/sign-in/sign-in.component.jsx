fc2804fdc51974235ec8f7fb1ab7c5decccaf953
Christus Arulanantham <>
2022-12-20 14:20:24 +0000

    Added diffrent authentication method examples

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,25 +1,35 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
 import {
+  auth,
   signInWithGooglePopup,
   singnInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils.js";
 
 const SignIn = () => {
+  useEffect(() => {
+    const googleRedirect = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+        // console.log(response);
+      }
+    };
+    googleRedirect();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
-  };
-
-  const logGoogleRedirectUser = async () => {
-    const { user } = await singnInWithGoogleRedirect();
-    console.log(user);
   };
 
   return (
     <div>
       <h1> Sign In Page </h1>
       <button onClick={logGoogleUser}>Sing In with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>
+      <button onClick={singnInWithGoogleRedirect}>
         Sing In with Google Redirect
       </button>
     </div>
